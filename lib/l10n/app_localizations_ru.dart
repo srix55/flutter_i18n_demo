50c8816bf490bae26d0dbf4164a590bd6f9ffbad
Srikanth Venkatesh <srix55@gmail.com>
2021-02-23 15:51:47 +0530

    Implemented flutter i18n docs

---
+++ lib/l10n/app_localizations_ru.dart
@@ -0,0 +1,14 @@
+
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+/// The translations for Russian (`ru`).
+class AppLocalizationsRu extends AppLocalizations {
+  AppLocalizationsRu([String locale = 'ru']) : super(locale);
+
+  @override
+  String get helloWorld => 'Привет, мир!';
+}
